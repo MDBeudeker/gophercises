0bbf7859980fbd1068b2a5292ee328061e2a8ce6
Maurits Beudeker <mauritsbeu@gmail.com>
2023-06-17 15:24:57 +0200

    Added the readline feature and the testing feature

--- quiz.go
+++ quiz.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "os"
+  "strings"
+  "bufio"
+)
+
+
+func check( e error){
+  if e != nil {
+    panic(e)
+  }
+}
 
 func main() {
-  fmt.Println("Hello world")
+  score := 0
+  fileName := "problems.csv"
+
+  dat, err := os.ReadFile(fileName)
+
+  check(err)
+
+  lines := strings.Split(string(dat), "\n")
+
+  for i := 0; i< len(lines)-1; i++ {
+    value := strings.Split(lines[i],",")
+    fmt.Print(value[0], "\n")
+    input := bufio.NewScanner(os.Stdin)
+    input.Scan()
+    if (input.Text() == value[1]){ //compare the user input to the correct answer
+      score ++
+      fmt.Print("that's Correct!!")
+    }   else{
+      fmt.Print("Wrong answer")
+    }
+      fmt.Print("\nYour Score is now ",score," out of ", len(lines)-1,"\n")
+  }
 }
